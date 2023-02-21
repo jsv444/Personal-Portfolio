36233f759ba374f69a7e184d3f455ddfe323c958
Josv <johannes.svanborg@outlook.com>
2023-02-21 23:46:53 +0100

    added navbar and some styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import "./header.css"
+
 function Header() {
   return (
     <div>
-      <div>
+      <div className="wrapper-header">
         <h1>Johannes Svanborg</h1>
         <h2>
           Full Stack Developer <br /> & <br /> Cloud Architect
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-import "./navbar.css"
-import mail from "../assets/mail.svg"
-import github from "../assets/github.svg"
-import linkedin from "../assets/linkedin.svg"
+import "./navbar.css";
+import mail from "../assets/mail.svg";
+import github from "../assets/github.svg";
+import linkedin from "../assets/linkedin.svg";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#32385A",
+    },
+  },
+});
 
 function Navbar() {
   return (
-    <div>
+    <div className="wrapper">
       <nav className="navbar">
         <ul className="navbar-menu">
           <li>
@@ -16,17 +26,24 @@ function Navbar() {
           <li>
             <a href="">ABOUT</a>
           </li>
-          <li>
-            <a href="">CONTACT ME</a>
+          <li className="button">
+            <ThemeProvider theme={theme}>
+              <Button color="primary" variant="contained">Contact Me</Button>
+            </ThemeProvider>
           </li>
-              </ul>
-              <ul className="socials">
-              <li><img src={mail} alt="" /></li>
-              <li><img src={github} alt="" /></li>
-              <li><img src={linkedin} alt="" /></li>
-          </ul>
-
-          </nav>
+        </ul>
+        <ul className="socials">
+          <li>
+            <img src={mail} alt="" />
+          </li>
+          <li>
+            <img src={github} alt="" />
+          </li>
+          <li>
+            <img src={linkedin} alt="" />
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
